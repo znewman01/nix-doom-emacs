d444888ce2dba8d2400c31d5ab5304562d3a5ba0
ckie <git-525ff67@ckie.dev>
2022-01-01 04:10:36 +0200

    .github: move it into update-dependencies.yml and finish up

--- .github/workflows/test_dep_update.sh
+++ .github/workflows/test_dep_update.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -i bash -p gh jq
 set -e
-echo "$GITHUB_API_TOKEN" | gh auth --with-token
+echo "$GITHUB_API_TOKEN" | gh auth login --with-token
 git branch -d depupdate || true
 git branch depupdate
 git checkout depupdate
 json="$(gh pr list --search "author:app/github-actions" --json headRefName,number)"
 echo "$json" | jq '.[] | .headRefName | @text' | xargs -L1 -- git pull --rebase origin
 if nix flake check; then
-    #echo "$json" | jq ".[] | .number | @text" | xargs gh pr merge
-    echo wouldve merged-all
+    echo "$json" | jq ".[] | .number | @text" | xargs -L1 -- gh pr merge --rebase --delete-branch
 else
     gh issue create \
         --title "Recent Dependency update PRs failing tests" \
