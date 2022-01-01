ce3c1ac7c49d3a17a382b480c7483604a6510356
ckie <git-525ff67@ckie.dev>
2022-01-01 04:06:09 +0200

    .github: goofing around with more automation

---
+++ .github/workflows/test_dep_update.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p gh jq
+set -e
+echo "$GITHUB_API_TOKEN" | gh auth --with-token
+git branch -d depupdate || true
+git branch depupdate
+git checkout depupdate
+json="$(gh pr list --search "author:app/github-actions" --json headRefName,number)"
+echo "$json" | jq '.[] | .headRefName | @text' | xargs -L1 -- git pull --rebase origin
+if nix flake check; then
+    #echo "$json" | jq ".[] | .number | @text" | xargs gh pr merge
+    echo wouldve merged-all
+else
+    gh issue create \
+        --title "Recent Dependency update PRs failing tests" \
+        --body "I tried to combine these PRs and they failed the test: $(echo "$json" | jq ".[] | .number | @text" | xargs echo '#')"
+fi
