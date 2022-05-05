971616be1ed899ae719d43e170cb98f8158c48a9
ckie <git-525ff67@ckie.dev>
2022-05-05 11:30:08 +0300

    .github: try hard-resetting too just incase

--- .github/workflows/test_dep_update.sh
+++ .github/workflows/test_dep_update.sh
@@ -5,8 +5,9 @@ echo "$GITHUB_API_TOKEN" | gh auth login --with-token
 git branch -D depupdate || true
 git branch depupdate
 git checkout depupdate
+git reset --hard origin/master
 json="$(gh pr list --search "author:app/github-actions" --json headRefName,number)"
-echo "$json" | jq '.[] | .headRefName | @text' | xargs -L1 -- git pull --rebase origin
+echo "$json" | jq '.[] | .headRefName | @text' | xargs -L1 -- git pull origin
 if nix flake check; then
     echo "$json" | jq ".[] | .number | @text" | xargs -L1 -- gh pr merge --squash --delete-branch
 else
