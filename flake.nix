920c99e161f646f0a1ad7ed366dc5ed5af04c9d2
Thiago Kenji Okada <thiagokokada@gmail.com>
2022-07-05 17:57:37 +0100

    Add hlissner/{evil-escape,ws-butler} to flake
    Those repositories are more up-to-date them upstream, and should include
    a few fixes.

--- flake.nix
+++ flake.nix
@@ -44,6 +44,8 @@
     emacs-overlay.flake = false;
     emacs-so-long.url = "github:hlissner/emacs-so-long";
     emacs-so-long.flake = false;
+    evil-escape.url = "github:hlissner/evil-escape";
+    evil-escape.flake = false;
     evil-markdown.url = "github:Somelauw/evil-markdown";
     evil-markdown.flake = false;
     evil-org-mode.url = "github:hlissner/evil-org-mode";
@@ -52,6 +54,8 @@
     evil-quick-diff.flake = false;
     explain-pause-mode.url = "github:lastquestion/explain-pause-mode";
     explain-pause-mode.flake = false;
+    format-all.url = "github:lassik/emacs-format-all-the-code/47d862d40a088ca089c92cd393c6dca4628f87d3";
+    format-all.flake = false;
     nix-straight.url = "github:nix-community/nix-straight.el";
     nix-straight.flake = false;
     nose.url = "github:emacsattic/nose";
@@ -60,10 +64,10 @@
     ob-racket.flake = false;
     org-contrib.url = "git+https://git.sr.ht/~bzg/org-contrib";
     org-contrib.flake = false;
-    org.url = "github:emacs-straight/org-mode";
-    org.flake = false;
     org-yt.url = "github:TobiasZawada/org-yt";
     org-yt.flake = false;
+    org.url = "github:emacs-straight/org-mode";
+    org.flake = false;
     php-extras.url = "github:arnested/php-extras";
     php-extras.flake = false;
     revealjs.url = "github:hakimel/reveal.js";
@@ -72,9 +76,8 @@
     rotate-text.flake = false;
     sln-mode.url = "github:sensorflo/sln-mode";
     sln-mode.flake = false;
-    format-all.url =
-      "github:lassik/emacs-format-all-the-code/47d862d40a088ca089c92cd393c6dca4628f87d3";
-    format-all.flake = false;
+    ws-butler.url = "github:hlissner/ws-butler";
+    ws-butler.flake = false;
 
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
