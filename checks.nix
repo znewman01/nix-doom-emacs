1713df2a43645a982e73b820235e4701b42dfbde
Thiago Kenji Okada <thiagokokada@gmail.com>
2022-07-08 19:46:36 +0100

    Add check for home-manager module

--- checks.nix
+++ checks.nix
@@ -9,8 +9,29 @@ let
     # this means we need to import the overlay in a hack-ish way
     overlays = [ (import emacs-overlay) ];
   };
+  home-manager = pkgs.fetchFromGitHub {
+    owner = "nix-community";
+    repo = "home-manager";
+    rev = "8160b3b45b8457d58d2b3af2aeb2eb6f47042e0f";
+    sha256 = "sha256-/aN3p2LaRNVXf7w92GWgXq9H5f23YRQPOvsm3BrBqzU=";
+  };
 in
 {
+  home-manager-module = (import "${home-manager}/modules" {
+    inherit pkgs;
+    configuration = {
+      imports = [ self.outputs.hmModule ];
+      home = {
+        username = "nix-doom-emacs";
+        homeDirectory = "/tmp";
+        stateVersion = "22.11";
+      };
+      programs.doom-emacs = {
+        enable = true;
+        doomPrivateDir = ./test/doom.d;
+      };
+    };
+  }).activationPackage;
   init-example-el = self.outputs.package.${system} {
     doomPrivateDir = ./test/doom.d;
     dependencyOverrides = inputs;
