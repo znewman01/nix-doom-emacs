d4f348c090f1f72dad8c5fdde9a65cc3b5dfebfc
hab25 <31970961+hab25@users.noreply.github.com>
2022-11-05 21:04:07 -0300

    Allow NixOS system info directory to be found

--- info-workarounds.el
+++ info-workarounds.el
@@ -1,3 +1,10 @@
 ;;; info-workarounds.el --- Description -*- lexical-binding: t; -*-
+;;; This file allows the `info` function to find other `info` files in the system.
+
+;;; For non-NixOS GNU/Linux
 (push "/usr/share/info" Info-directory-list)
+
+;;; For NixOS
+(push "/run/current-system/sw/share/info" Info-directory-list)
+
 (push "~/.nix-profile/share/info" Info-directory-list)
